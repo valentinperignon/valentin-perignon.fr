b56ffaa6272f92764d5dd27754f2d03edef78815
Valentin Perignon <valentin.perignon@orange.fr>
2020-12-27 17:23:59 +0100

    Update tests

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("nav ul").addEventListener("click", (ev) => {
-    const target = ev.target;
-
-    if (target.dataset.name) {
-      if (target.classList.contains("selected")) return;
-
-      const last = document.querySelector("button.selected");
-      last.classList.remove("selected");
-      target.classList.add("selected");
-
-      document
-        .getElementById(last.dataset.name)
-        .classList.remove("display-content");
-      document
-        .getElementById(target.dataset.name)
-        .classList.add("display-content");
-    }
-  });
-});
-
+// Hey!
 console.log(
   "Hey! I am looking for an internship from 04-06-21 :)\nFeel free to contact me!"
 );
+
+// Tabs
+document.querySelector("nav ul").addEventListener("click", ev => {
+  const target = ev.target;
+
+  if (target.dataset.name) {
+    if (target.classList.contains("selected")) return;
+
+    const last = document.querySelector("button.selected");
+    last.classList.remove("selected");
+    target.classList.add("selected");
+
+    document
+      .getElementById(last.dataset.name)
+      .classList.remove("display-content");
+    document
+      .getElementById(target.dataset.name)
+      .classList.add("display-content");
+  }
+});
--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -1,56 +1,67 @@
 require "selenium-webdriver"
 require "net/http"
 
-driver = nil
-wait = nil
+$driver = nil
+$wait = nil
+$links_tested = []
 
 RSpec.describe "User Interface" do
-  def check_url(driver, expected)
-    current_url = driver.current_url
+  # Functions
 
-    uri = URI.parse current_url
-    res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == "https") do |http|
-      req = Net::HTTP::Get.new uri
-      result = http.request req
-      result.code == "200"
+  def check_url(expected)
+    current_url = $driver.current_url
+
+    res = true
+    if !$links_tested.include?(current_url)
+      uri = URI.parse current_url
+      res = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == "https") do |http|
+        req = Net::HTTP::Get.new uri
+        result = http.request req
+        result.code != "404"
+      end
+      $links_tested.push(current_url)
     end
 
     current_url =~ /#{expected}/ && res
   end
 
-  def click_ext_link(driver, num)
-    driver.find_element(:css, "header > #title > ul > li:nth-of-type(#{num+1}) > a").click
+  def click_ext_link(num)
+    $driver.find_element(:css, "header > #title > ul > li:nth-of-type(#{num+1}) > a").click
   end
 
-  def get_current_tab(driver)
-    driver.find_element(:css, "button.selected").text
+  def get_current_tab
+    $driver.find_element(:css, "button.selected").text
   end
 
-  def change_tab(driver, num)
-    driver.find_element(:css, "main > nav li:nth-of-type(#{num+1}) > button").click
+  def change_tab(num)
+    $driver.find_element(:css, "main > nav li:nth-of-type(#{num+1}) > button").click
   end
+
+  # Before and after
 
   before(:each) do
-    driver = Selenium::WebDriver.for :chrome
-    driver.get "http://127.0.0.1:4000"
-    wait = Selenium::WebDriver::Wait.new :timeout => 10
+    $driver = Selenium::WebDriver.for :chrome
+    $driver.get "http://127.0.0.1:4000"
+    $wait = Selenium::WebDriver::Wait.new :timeout => 10
   end
 
   after(:each) do
-    driver.quit
+    $driver.quit
   end
+
+  # Tests
 
   it "checks the three tabs" do
     # check default tab
-    first_tab = driver.find_element(:css, "main > nav li:first-of-type > button").text
-    default_tab = get_current_tab(driver)
+    first_tab = $driver.find_element(:css, "main > nav li:first-of-type > button").text
+    default_tab = get_current_tab
     expect(first_tab).to eq(default_tab)
 
     # navigate between tabs
     next_tab = default_tab
-    [1, 2, 0].each do |tab|
-      change_tab(driver, tab)
-      tmp = get_current_tab(driver)
+    [1, 2, 0].each do |n|
+      change_tab n
+      tmp = get_current_tab
       expect(next_tab).to_not eq(tmp)
       next_tab = tmp
     end
@@ -59,25 +70,25 @@ RSpec.describe "User Interface" do
   it "checks the external links in the header" do
     links = ["resume.pdf", "linkedin.com", "github.com", "codepen.io"]
     links.length.times do |n|
-      wait.until do driver.find_element(:css, "header > #title > ul > li:nth-of-type(1) > a") end
-      click_ext_link(driver, n)
-      expect(check_url(driver, links[n])).to_not eq(nil)
-      driver.navigate.back
+      $wait.until do $driver.find_element(:css, "header > #title > ul > li:nth-of-type(1) > a") end
+      click_ext_link n
+      expect(check_url(links[n])).to eq(true)
+      $driver.navigate.back
     end
   end
 
   it "checks the links on the Project tab" do
     links = ["schoolexams", "github", "codepen"]
-    change_tab(driver, 1)
-    documentLinks = driver.find_elements(:css, ".display-content a")
+    change_tab 1
+    documentLinks = $driver.find_elements(:css, ".display-content a")
     expect(links.length).to eq(documentLinks.length)
 
     links.length.times do |n|
-      all = driver.find_elements(:css, ".display-content a")
+      all = $driver.find_elements(:css, ".display-content a")
       all[n].click
-      expect(check_url(driver, links[n])).to_not eq(nil)
-      driver.navigate.back
-      change_tab(driver, 1)
+      expect(check_url(links[n])).to eq(true)
+      $driver.navigate.back
+      change_tab 1
     end
   end
 end
